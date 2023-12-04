010ec27a2400ca42241dd4beb96a11f470f4762e
davidenders11 <david.enders@xostrucks.com>
2023-12-03 19:43:17 -0800

    add to training notebook

--- vae.py
+++ vae.py
@@ -21,11 +21,3 @@ class Basic_VAE(nn.Module):
     # The loss must include a measure of the difference between the input image and reconstructed image
     # as well as a measure of the difference between the normals we get from mu and log_var and the standard
     # normal by using kl divergence
-    def loss_function(self, reconstructed_img, input_img, mu, log_var, kld_weight=2):
-        img_loss = F.mse_loss(reconstructed_img, input_img)
-        # article on calculating kl divergence between 2 gaussians:
-        # https://medium.com/@outerrencedl/variational-autoencoder-and-a-bit-kl-divergence-with-pytorch-ce04fd55d0d7 
-        kld_loss = torch.mean(torch.sum((0-log_var) +  (log_var.exp()-mu**2)/2 - 1/2))
-        kld_loss *= kld_weight
-
-        return img_loss + kld_loss
