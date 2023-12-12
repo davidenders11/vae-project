05b66f35d8c7de2d8444ed962365a235fdecbcee
JoshuaG-K <jgarciakimble@g.hmc.edu>
2023-12-11 22:54:51 -0800

    Added code to display images after model is trained

--- vae.py
+++ vae.py
@@ -9,6 +9,7 @@ from decoder import Decoder
 class Basic_VAE(nn.Module):
     def __init__(self, in_dim, hidden_dims, latent_dim):
         super().__init__()
+        self.latent_dim = latent_dim
         self.encoder = Encoder(in_dim, hidden_dims, latent_dim)
         self.decoder = Decoder(latent_dim, hidden_dims)
 
@@ -17,6 +18,15 @@ class Basic_VAE(nn.Module):
         reconstructed_img = self.decoder(mu, log_var)
         return [reconstructed_img, mu, log_var]
 
+    def sample_img(self):
+        zeros = torch.zeros((self.latent_dim, 1))
+        ones = torch.ones((self.latent_dim, 1))
+
+        normal_input = torch.normal(zeros, ones)
+        normal_input = normal_input.view(1, self.latent_dim)
+        print(normal_input.shape)
+        sampled_img = self.decoder.decode(normal_input)
+        return sampled_img
     # The loss must include a measure of the difference between the input image and reconstructed image
     # as well as a measure of the difference between the normals we get from mu and log_var and the standard
     # normal by using kl divergence
